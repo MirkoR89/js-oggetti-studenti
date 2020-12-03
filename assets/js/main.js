e3f72d83d63d0d01aa58931fe0ac5b1e1b85d9a7
Mirko <mirko.rinelli@gmail.com>
2020-12-03 16:03:44 +0100

    Arry of object

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 var studente = {
   "nome": "Mirko",
   "cognome": "Rinelli",
-  "età": "28"
+  "età": "31"
 }
 /*console.log(studente.nome);
 console.log(studente["cognome"]);*/
@@ -10,6 +10,25 @@ console.log(studente["cognome"]);*/
 for (var key in studente) {
   console.log(studente[key]);
 }
+
 //Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+var listaStudenti = [
+{
+  "nome": "Mirko",
+  "cognome": "Rinelli",
+  "età": "31"
+},
+{
+  "nome": "Mario",
+  "cognome": "Rossi",
+  "età": "25"
+},
+{
+  "nome": "Luigia",
+  "cognome": "Bianchi",
+  "età": "55"
+},
+];
+
 
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
